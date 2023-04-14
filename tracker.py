fe1f0efb23cfc51e43ff2d6811c45a16e51b2084
ANONYMOUS | MOJO <130356338+Anonymousnism@users.noreply.github.com>
2023-04-14 22:41:35 +0700

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -4,10 +4,6 @@ from phonenumber import geocoder, carrier, timezone
 
 number = input ("enter your number: ")
 
-print
-print " Tools Hack By Mojo Anonymous "
-print
-
 phone_number = phonenumber.parse(number)
 
 print(f"Location: {geocoder.description_for_number(phone_number, 'en')}")
