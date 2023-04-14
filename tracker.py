1cbca29e5a83665e067914eaebebccd2d0e7221d
ANONYMOUS | MOJO <130356338+Anonymousnism@users.noreply.github.com>
2023-04-14 22:16:03 +0700

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -8,6 +8,6 @@ phone_numbers = phonenumbers.parse(number)
 
 print(f"Location {geocoder.description_for_number(phone_number, 'en')}")
 
-print(f"Carier) {carier.name_for_number(phone_number, 'en')} ")
+print(f"Carier) {carrier.name_for_number(phone_number, 'en')} ")
 
 print(f"Time Zone {timezone.Time_Zones_for_number(phone_number)}")
